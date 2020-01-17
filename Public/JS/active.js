b96a9322c45218109d633a3b7ae1212903dcd0e2
Alban HUSAR <ahusar@561557n.ac-rennes.fr>
2020-01-17 11:04:19 +0100

    maj bootstrap

--- Public/JS/active.js
+++ Public/JS/active.js
@@ -1,6 +1,4 @@
-function menuActive(indexActive){
-    let menu = document.getElementById('menuactive');//RECUPERE MES UL AVEC L'ID
-    let element = menu.getElementsByClassName('nav-item');//RECUPERE MES LI AVEC LA CLASS
-
-    element[indexActive].className += " active";//LA FONCTION ACTIVE LA PAGE SUR LAQUELLE ON SE TROUVE RAPPELER ENSUITE DANS LE CSS POUR Y METTRE DE LA COULEUR
+function linksActive(indexActive) {
+    let links = document.getElementsByClassName('nav-link');
+    links[indexActive].className +=" active";
 }
